43ec3f53b1ac4d98dd099e1f16b3c861f9e944fb
EliasMenn <eliasmennella10@gmail.com>
2024-09-09 19:01:19 -0300

    Signed-off-by: EliasMenn <eliasmennella10@gmail.com>

---
+++ TP_Topicos_2024_2c_Jueves_MemManiacs/Headers.h
@@ -0,0 +1,6 @@
+#ifndef HEADERS_H_INCLUDED
+#define HEADERS_H_INCLUDED
+
+
+
+#endif // HEADERS_H_INCLUDED
---
+++ TP_Topicos_2024_2c_Jueves_MemManiacs/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "headers.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
